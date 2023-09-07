2558c16ee813873fbc8388c8f08cdc864081dfea
juanj9805 <juanjoseb9805@gmail.com>
2023-09-07 12:01:24 -0600

    trabajo mañana

--- main.js
+++ main.js
@@ -12,9 +12,22 @@ toggle.addEventListener( 'click' , () =>{
     sidebar.classList.toggle("close");
 } )
 
-searchBtn.addEventListener( 'click', () => {
+
+
+
+    searchBtn.addEventListener( 'click', () => {
+        sidebar.classList.remove("close");
+        imglogo__bt.style.display="none"
+    } )
+
+
+   
+
+/* searchBtn.addEventListener('click', () => {
     sidebar.classList.remove("close");
-} )
+    imglogo__bt.style.display = "none";
+}); */
+
 
 modeSwitch.addEventListener('click', ()=>{
 
@@ -37,6 +50,8 @@ modeSwitch.addEventListener('click', ()=>{
 let menuHamburguesa = document.getElementById("menuHamburguesa")
 let parteDerecha = document.getElementById("parteDerecha")
 let estadoBoton = 0;
+let imglogo__bt = document.getElementById("logo__bt")
+
 
 menuHamburguesa.addEventListener("click", function(){
 /*     alert("hola compas");
@@ -47,10 +62,12 @@ menuHamburguesa.addEventListener("click", function(){
 function actualizarEstado() {
     if (estadoBoton % 2 === 0) {
         parteDerecha.style.width="92%";
+        imglogo__bt.style.display="none";
 
         // Realiza acciones correspondientes al estado par
     } else {
         parteDerecha.style.width="77.5%";
+        imglogo__bt.style.display="block";
 
         // Realiza acciones correspondientes al estado impar
     }
